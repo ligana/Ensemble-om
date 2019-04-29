b601697b7596e3135a2ab3337c0285e0e2ee347a
jiajt <jiajt@dcits.com>
2019-04-29 16:56:29 +0800

    提交报错

--- src/views/prodFactory/prodInfo/soldProd/SoldProd.vue
+++ src/views/prodFactory/prodInfo/soldProd/SoldProd.vue
@@ -30,7 +30,7 @@
                     </v-layout>
                     <v-layout row wrap>
                         <v-flex v-for="(keyData ,key ,index) in dataSource" v-bind:key="key" lg6>
-                            <v-layout row wrap  v-if="keyData.columnType == 'PART'">
+                            <v-layout row wrap v-if="keyData.columnType == 'PART'">
                                 <v-flex md12 lg12>
                                     <dc-part :disablePower="disablePower" :showEdit="showEdit" v-if="keyData.columnType == 'PART'" :data="keyData"></dc-part>
                                 </v-flex>
